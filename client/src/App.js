68e31c0fa7c1f04c18e1d41a6a652e095b864682
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-09-14 21:21:35 -0400

    playing around with column and row layouts on the App.js page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-     <div>
-       <h1>this is going to be a timer app for HIIT workouts</h1>
-     </div>
+     <Container>
+     <Row>
+      <Col> <h1>This is going to be </h1> </Col>
+      <Col> <h1>A Timer App for HIIT workouts</h1> </Col>
+     </Row>
+     </Container>
     </div>
   );
 }
